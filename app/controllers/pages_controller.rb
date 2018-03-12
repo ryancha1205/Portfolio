720f1f38405cdb57baad9b66cb19d90c7c7018fc
herkas413 <herkas413@naver.com>
2018-03-12 18:36:10 +0900

    Built pages via controller generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
-    @skills = Skill.all
+    #fetch data from Blog Model to the controller 
+    #so the view file can fetch the data  #MCV
+    #posts become an instance variable
   end
 
   def about
